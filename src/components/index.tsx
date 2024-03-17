eb685cf642efe7bf8d46bcddd0c9499a29079022
yessine-agrebi <yessine.agrebi.aiesec@gmail.com>
2024-03-17 15:45:50 +0100

    adding exports

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,5 @@ export * from "./Calendar";
 export * from "./DayHeader";
 export * from "./TimeSlot";
 export * from "./CalendarHead";
+export * from "./CalendarBody";
+export * from "./CalendarFooter";
